a98e4d8a72f3b8f00812cb08bb94be2bf0bd96d6
Adilson Gregorio da Silva <98970063+Theoldwine@users.noreply.github.com>
2022-04-07 23:52:58 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# package_name
+# Dio_images
 
-Description. 
-The package package_name is used to:
-	- 
-	-
+Description.
+
+The package Dio_images is used to:
+
+	Processing:
+		- Histogram matching
+		- Structural similatiy
+		- Resize Imagem
+	Utils:
+		- Road imagem
+		- Save Imagem
+		- Plot Image
+		- Plot result
+		- Plot hitogram 
 
 ## Installation
 
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
 
 ```bash
-pip install package_name
+pip install image_processing
 ```
 
 ## Usage
@@ -21,7 +31,7 @@ file1_name.my_function()
 ```
 
 ## Author
-My_name
+Th
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    name="Dio_Images",
+    version="0.0.2",
+    author="theoldwine",
+    author_email="sior@outlook.com",
+    description="projeto python leitura da imagem",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Theoldwine/package-template",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
